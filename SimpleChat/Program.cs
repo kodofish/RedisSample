8638a12db2c2c91f60fb2887a9531cc695339023
STACKCORE\yuwei.chang <yuwei.chang@stackcore.com>
2024-05-03 16:10:52 +0800

    Add SimpleChat project
    The commit introduces a SimpleChat project that demonstrates the use of Redis as a message broker. It connects to Redis, subscribes to a "chat" channel, and handles incoming messages. It also includes configuration for the .NET 8.0 SDK, the inclusion of the 'StackExchange.Redis' package, and files for solution and ignore settings in Visual Studio.

---
+++ SimpleChat/Program.cs
@@ -0,0 +1,46 @@
+﻿using StackExchange.Redis;
+
+namespace SimpleChat;
+
+class Program
+{
+    private static readonly ConnectionMultiplexer Multiplexer = ConnectionMultiplexer.Connect("localhost");
+    private static readonly Queue<string> Messages = new();
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Enter your name：");
+        var userName = Console.ReadLine() ?? "Anonymous";
+
+        var subscriber = Multiplexer.GetSubscriber();
+        
+        // 訂閱 chat 頻道
+        await subscriber.SubscribeAsync("chat", (channel, message) =>
+        {
+            Messages.Enqueue($"{message}");
+            DisplayMessages(Messages);
+        });
+
+        // 發佈訊息
+        await subscriber.PublishAsync("chat", $"The user `{userName}` join to chat room.");
+
+        while (true)
+        {
+            // 讀取使用者輸入的訊息並發佈到 chat 頻道
+            var userMessage = Console.ReadLine();
+            await subscriber.PublishAsync("chat", $"{userName}: {userMessage}");
+        }
+    }
+
+    private static void DisplayMessages(Queue<string> messages)
+    {
+        Console.Clear();
+        if (messages.Count > 20)
+            messages.Dequeue();
+        
+        foreach (var message in messages.ToArray())
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
