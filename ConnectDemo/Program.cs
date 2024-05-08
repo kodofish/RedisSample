7206c8cbf00e0d1af58351a960259bfe947e86c7
kodofish <kodofish@gmail.com>
2024-05-09 00:07:44 +0800

    Add ConnectDemo project to solution
    Added a new ConnectDemo project to the solution which includes the main program file and project configuration file. This program uses the StackExchange.Redis library to establish a connection to a Redis instance on localhost.

---
+++ ConnectDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+namespace ConnectDemo;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var options = new ConfigurationOptions
+        {
+            EndPoints = { "localhost:6379" },
+            Ssl = false,
+            AbortOnConnectFail = false
+        };
+        
+        var textWriter = new StringWriter();
+        
+        await ConnectionMultiplexer.ConnectAsync(options, textWriter);
+        Console.WriteLine(textWriter);
+        Console.ReadLine();
+
+    }
+}
