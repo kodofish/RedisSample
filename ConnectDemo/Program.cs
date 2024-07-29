6c4701515289d1e2dcb2d5275f9295cd9f508fdb
STACKCORE\yuwei.chang <yuwei.chang@stackcore.com>
2024-07-29 14:00:17 +0800

    Add Redis subscription and message publishing logic
    Integrated Redis subscription to "chat" channel and implemented message publishing with connection status checks and error handling. This enhances the functionality by enabling real-time message handling and validating connectivity with Redis.

--- ConnectDemo/Program.cs
+++ ConnectDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using StackExchange.Redis;
+﻿using System.Diagnostics;
+using StackExchange.Redis;
 
 namespace ConnectDemo;
 
@@ -12,12 +13,44 @@ class Program
             Ssl = false,
             AbortOnConnectFail = false
         };
-        
-        var textWriter = new StringWriter();
-        
-        await ConnectionMultiplexer.ConnectAsync(options, textWriter);
-        Console.WriteLine(textWriter);
-        Console.ReadLine();
 
+        var textWriter = new StringWriter();
+
+        var connect = await ConnectionMultiplexer.ConnectAsync(options, textWriter);
+        Console.WriteLine(textWriter);
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
+        await textWriter.FlushAsync();
+        connect.GetSubscriber().Subscribe("chat",
+            (channel, message) => Console.WriteLine($"Received message: {message} on channel: {channel}"));
+
+
+        while (true)
+        {
+            var sw = new Stopwatch();
+            try
+            {
+                
+                Console.WriteLine($"Is connected : {connect.IsConnected}");
+                Console.WriteLine($"Is connecting: {connect.IsConnecting}");
+                // subscriber.Publish("chat", "Hello, World!");
+                sw.Start();
+                var subscriber = connect.GetSubscriber();
+                subscriber.Publish("chat", "Hello, World!");
+                var db = connect.GetDatabase();
+                var timespan = db.Ping();
+                Console.WriteLine($"Message sent. Ping: {timespan.TotalMilliseconds}ms");
+            }
+            catch (Exception e)
+            {
+                sw.Stop();
+                Console.WriteLine(e.GetType().FullName);
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine($"Press Enter to continue...{sw.ElapsedMilliseconds} ms");
+            }
+
+            Console.ReadLine();
+        }
     }
 }
